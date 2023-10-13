3429e0bc52533de61f5d2ca3e74351c41ad3c4ee
juligarc91 <julissagarcia@pursuit.org>
2023-10-13 14:51:10 -0400

    Installed and imported to index.js npm package faker

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Just-a-Regular-Flower-Shop
 ``` js
-lolcats.print('Lorem ipsum dolor sit amet') // to use colorfultext for special occassion flowers
+lolcats.print('Lorem ipsum dolor sit amet')
+chalkAnimation.rainbow('Lorem ipsum dolor sit amet');
+// to use colorfultext for special occassion flowers
 ```
--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const lolcats = require("lolcats"); // importing lolcats mod
+// --- Import NPM Packages ---
+//const lolcats = require("lolcats"); // importing lolcats mod (not supported after changing package.json line 6 to "type": "module")
+//const chalkAnimation = require('chalk-animation'); // gives error message code 'ERR_REQUIRE_ESM' to fix add to package.json "type": "module"
+//const { faker } = require("@faker-js/faker");
 
-lolcats.print('Lorem ipsum dolor sit amet')
+import chalkAnimation from 'chalk-animation'; // to import chalk-animation w/o error
+import lolcats from 'lolcats'; // to import lolcats w/o error after changing package.json
+import { faker } from '@faker-js/faker'; // to import faker w/o error after changing package.json
 
+
+// --- Test if import of npm packages worked ---
+//lolcats.print('Lorem ipsum dolor sit amet')
+//chalkAnimation.rainbow('Lorem ipsum dolor sit amet');
+const material = {
+    material: faker.commerce.productMaterial(), // generates string
+};
+console.log (material); // generates object
+
+const run = () => {};
+
+run ();
